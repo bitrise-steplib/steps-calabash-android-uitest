f639decac27b3d5cbc44a81e668293961562dbf6
Krisztián Gödrei <godrekrisztian@gmil.com>
2015-11-26 11:16:53 +0000

    fix

--- step.sh
+++ step.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -e
 
-current_path=$(pwd)
-cd $THIS_SCRIPT_DIR
-bundle exec ruby "step.rb" \
+# current_path=$(pwd)
+# cd $this_script_dir
+# bundle exec ruby "step.rb" \
+	# -a "${calabash_features}" \
+	# -b "${apk_path}"
+# cd $current_path
+
+ruby "$this_script_dir/step.rb" \
 	-a "${calabash_features}" \
 	-b "${apk_path}"
-cd $current_path
