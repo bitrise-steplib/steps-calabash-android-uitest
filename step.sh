bc3b8acae0b378f9a7519df5dbe1c3e002e39955
birmacher <birmacher@gmail.com>
2015-12-15 16:34:53 +0100

    removed checking for calabash feature directory

--- step.sh
+++ step.sh
@@ -4,10 +4,7 @@ this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -e
 
-current_path=$(pwd)
-cd $this_script_dir
 bundle install
-bundle exec ruby "step.rb" \
+bundle exec ruby "${this_script_dir}/step.rb" \
 	-a "${calabash_features}" \
 	-b "${apk_path}"
-cd $current_path
