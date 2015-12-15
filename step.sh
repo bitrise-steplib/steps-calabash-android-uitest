8c98a48737aa05d89e3f15837fe28cc6fc2e62d4
birmacher <birmacher@gmail.com>
2015-12-15 16:34:53 +0100

    calabash fix (+5 squashed commits) Squashed commits: [ef39b1b] gemfeile fixes [d3a715e] bugfix [01d27f1] generate debug keystore if not available [da39cfe] bugfix [bc3b8ac] removed checking for calabash feature directory

--- step.sh
+++ step.sh
@@ -4,10 +4,8 @@ this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -e
 
-current_path=$(pwd)
-cd $this_script_dir
+export BUNDLE_GEMFILE="${this_script_dir}/Gemfile"
+
 bundle install
-bundle exec ruby "step.rb" \
-	-a "${calabash_features}" \
+bundle exec ruby "${this_script_dir}/step.rb" \
 	-b "${apk_path}"
-cd $current_path
